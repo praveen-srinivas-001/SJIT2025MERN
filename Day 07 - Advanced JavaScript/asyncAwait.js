257640e8f991b86ea12d2f2a6ef5ed01dfcadfa5
praveen-srinivas-001 <praveensrinivasan521@gmail.com>
2025-02-18 22:16:17 +0530

    Day-8: Completed Async and Await, DOM manipulation (18-02-2025 FN)

--- Day 07 - Advanced JavaScript/asyncAwait.js
+++ Day 07 - Advanced JavaScript/asyncAwait.js
@@ -1,9 +1,18 @@
 async function asaw() {
+    var a = 10;
     setTimeout(() => {
         console.log('hello world');
     }, 5000);
+    console.log(a);
 }
 console.log(asaw());
+
+
+// asaw().then((res) => {
+//     console.log(res);
+// }).catch((err) => {
+//     console.log(err);
+// });
 
 // npm - Node Package Manager
 // npx - Node Package Executor
