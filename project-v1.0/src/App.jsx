43b3dcfc5db99e5eacb2539e148c9ea2ef77260a
praveen-srinivas-001 <praveensrinivasan521@gmail.com>
2025-02-19 23:20:06 +0530

    Day 09 - Worked on react-router-dom  BrowserRouter, Routes, Route (19-02=2925 AN)

--- project-v1.0/src/App.jsx
+++ project-v1.0/src/App.jsx
@@ -1,16 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
+import Home from "./components/FunctionalComponent/Home.jsx";
+import About from "./components/FunctionalComponent/About.jsx";
+import Contact from "./components/FunctionalComponent/Contact.jsx";
+import Gallery from "./components/FunctionalComponent/Gallery.jsx";
+import Signup from "./components/FunctionalComponent/Signup.jsx";
+import Navbar from "./components/FunctionalComponent/Navbar.jsx";
+import UseEffect from "./components/FunctionalComponent/UseEffects.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  
   return (
     <div>
-      <h1>Welcome to react</h1>
-      <home></home>
+      <main>
+        <BrowserRouter>
+          <Navbar /> {/* navbar should be inside the browser router tag because as the navbar uses the Link from BrowserRouter*/}
+          <Routes>
+            <Route path="/" element={<Home sjit="st.joseph" />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+        </BrowserRouter>
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
