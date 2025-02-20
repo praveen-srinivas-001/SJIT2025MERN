e1e457c5ab91775bdbc81692c9221115d3aced36
praveen-srinivas-001 <praveensrinivasan521@gmail.com>
2025-02-20 23:06:13 +0530

    Day-10: Hooks continuation (useMemo) Completed (20.02.2025 AN)

--- project-v1.0/src/App.jsx
+++ project-v1.0/src/App.jsx
@@ -5,7 +5,11 @@ import Contact from "./components/FunctionalComponent/Contact.jsx";
 import Gallery from "./components/FunctionalComponent/Gallery.jsx";
 import Signup from "./components/FunctionalComponent/Signup.jsx";
 import Navbar from "./components/FunctionalComponent/Navbar.jsx";
-import UseEffect from "./components/FunctionalComponent/UseEffects.jsx";
+import UseEffect from "./components/FunctionalComponent/Hooks/UseEffects.jsx";
+import UseState from "./components/FunctionalComponent/Hooks/UseState.jsx";
+import UseEffectAPI from "./components/FunctionalComponent/Hooks/UseEffectAPI.jsx";
+import UseRef from "./components/FunctionalComponent/Hooks/UseRef.jsx";
+import UseMemo from "./components/FunctionalComponent/Hooks/UseMemo.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -13,11 +17,17 @@ function App() {
     <div>
       <main>
         <BrowserRouter>
-          <Navbar /> {/* navbar should be inside the browser router tag because as the navbar uses the Link from BrowserRouter*/}
+          <Navbar />{" "}
+          {/* navbar should be inside the browser router tag because as the navbar uses the Link from BrowserRouter*/}
           <Routes>
             <Route path="/" element={<Home sjit="st.joseph" />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
+            <Route path="/use-effect" element={<UseEffect />} />
+            <Route path="/use-state" element={<UseState />} />
+            <Route path="/use-effect-api" element={<UseEffectAPI />} />
+            <Route path="/use-ref" element={<UseRef />} />
+            <Route path="/use-memo" element={<UseMemo />} />
             <Route path="/gallery" element={<Gallery />} />
             <Route path="/signup" element={<Signup />} />
           </Routes>
